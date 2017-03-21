fd03f6934913f9737665d9e7500e456a77758062
99hsueh <99hhsueh@gmail.com>
2017-03-21 18:44:32 +0000

    setup capybara and poltergeist

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,21 @@ class User < ActiveRecord::Base
   has_many :orders
   has_many :reviews
 
+  def self.authenticate_with_credentials(email, password)
+    email = email.downcase.strip
+    user = User.find_by(email: email)
+    if user && user.authenticate(password)
+      user
+    end
+  end
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email,
             presence: true,
-            uniqueness: { message: "This email has already been registered!" }
+            format: { without: /\s/ },
+            uniqueness: { case_sensitive: false, message: "This email has already been registered!" }
+  validates :password,
+            presence: true,
+            length: { in: 5..20 }
 end
